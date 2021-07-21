7e0c183ca5c2d82e0d1efa75fc74b6c0fd29e476
Luis Gustavo Bier Schuler <luis.schuler@gmail.com>
2021-07-21 16:05:18 -0300

    feat: create vue SampleComponent

---
+++ src/app/components/SampleComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="component-content">
+    {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SampleComponent',
+  props: {
+    msg: String,
+  },
+});
+</script>
